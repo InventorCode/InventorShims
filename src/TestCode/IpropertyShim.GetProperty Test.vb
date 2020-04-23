3cf196866e702fa6128702fb0c35d998a4a0216b
mjordan <meloncholia@gmail.com>
2020-04-22 23:12:58 -0400

    Renamed

--- src/TestCode/IpropertyShim.GetProperty Test.vb
+++
@@ -1,9 +0,0 @@
-﻿AddVbFile "IpropertyShim.vb"
-
-'Run the ipropertyShim functions from inventor's ilogic environment.
-Sub Main()
-
-    Dim doc As Inventor.Document = ThisApplication.ActiveDocument
-    MsgBox(IpropertyShim.GetProperty(doc, "Title"))
-
-End Sub
---
+++ src/TestCode/PropertyShim.GetProperty Test.vb
@@ -0,0 +1,8 @@
+﻿AddVbFile "PropertyShim.vb"
+
+'Run the PropertyShim functions from inventor's ilogic environment.
+Sub Main()
+
+    MsgBox(PropertyShim.GetProperty(ThisApplication.ActiveDocument, "Title"))
+
+End Sub
