686be9e378dbd8c01f94278fb4fdfd2523a96056
mjordan <meloncholia@gmail.com>
2020-04-22 23:13:08 -0400

    Renamed

---
+++ src/TestCode/PropertyShim.GetProperty SpeedTest.vb
@@ -0,0 +1,39 @@
+AddVbFile "PropertyShim.vb"
+AddVbFile "TestCode/SpeedTest.vb"
+AddVbFile "TestCode/Timer.vb"
+
+'This is a speed test between several methods to access all iproperties within a propertyset.
+'   Results: accessing all properties in a propertyset with GetPropertyInfo is faster than
+'           iterating through all properties within that propertyset.
+
+'Module SpeedTestMain '<CollectorPrepend>'</CollectorPrepend>
+    Public Sub Main ()
+        
+        Dim test As SpeedTest = New SpeedTest()
+
+        test.AddTest("PropertyShim.GetProperty", AddressOf Test1)
+        test.AddTest("Inventor API", AddressOf Test2)
+
+        test.Iterations = 100
+        test.RunTests()
+        test.ShowResultsInDialog()
+
+    End Sub
+
+
+    Public Sub Test1()
+        Dim _doc As Inventor.Document = ThisApplication.ActiveDocument
+        Dim value As String
+        value = PropertyShim.GetProperty(_doc, "Part Number")
+
+    End Sub
+
+    Public Sub Test2()
+        Dim _doc As Inventor.Document = ThisApplication.ActiveDocument
+        Dim propertySet As Inventor.PropertySet = _doc.PropertySets.Item("Design Tracking Properties")
+        Dim value As String
+        value = propertySet.Item("Part Number").Value
+
+    End Sub
+
+'    End Module '<CollectorPrepend>'</CollectorPrepend>
