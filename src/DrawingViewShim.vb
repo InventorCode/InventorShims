606f7d0e589412ab914d2734162ed026a69e7da1
mjordan <meloncholia@gmail.com>
2020-05-25 00:02:45 -0400

    Added DrawingViewShim

---
+++ src/DrawingViewShim.vb
@@ -0,0 +1,52 @@
+﻿Imports Inventor
+
+Public Class DrawingViewShim
+
+    Public View As Inventor.DrawingView
+
+    Public Sub New (view As Inventor.DrawingView)
+        View = view
+    End Sub
+
+    'TODO: make these set as well as get?
+    Public Function UpperLeft() As Inventor.Point2d
+        Dim transientGeometry As Inventor.TransientGeometry = ThisApplication.TransientGeometry
+
+        Dim x As Double = View.Left
+        Dim y As Double = View.Top
+        Dim point As Inventor.Point2d = transientGeometry.CreatePoint2d(x, y)
+        Return point
+
+    End Function
+    
+    Public  Function UpperRight() As Inventor.Point2D
+        Dim transientGeometry As Inventor.TransientGeometry = ThisApplication.TransientGeometry
+
+        Dim x As Double = View.Left+View.Width
+        Dim y As Double = View.Top
+        Dim point As Inventor.Point2d = transientGeometry.CreatePoint2d(x, y)
+        Return point
+
+    End Function
+
+    Public Function LowerLeft() As Inventor.Point2d
+        Dim transientGeometry As Inventor.TransientGeometry = ThisApplication.TransientGeometry
+
+        Dim x As Double = View.Left
+        Dim y As Double = View.Top+View.Height
+        Dim point As Inventor.Point2d = transientGeometry.CreatePoint2d(x, y)
+        Return point
+
+    End Function
+    Public  Function LowerRight() As Inventor.Point2D
+        Dim transientGeometry As Inventor.TransientGeometry = ThisApplication.TransientGeometry
+
+        Dim x As Double = View.Left+View.Width
+        Dim y As Double = View.Top+View.Height
+        Dim point As Inventor.Point2d = transientGeometry.CreatePoint2d(x, y)
+        Return point
+    End Function
+
+
+
+End Class
