a9d7dacb7d6848b6362c96fcdfbb605ab5cd561a
mjordan <meloncholia@gmail.com>
2020-06-26 19:56:41 -0400

    Added DocumentShim

---
+++ src/DocumentShim.vb
@@ -0,0 +1,80 @@
+﻿'Imports System.Runtime.CompilerServices
+
+Public Class DocumentShim
+
+    Shared Function IsPart(document As Inventor.Document) As Boolean
+    
+        If document.DocumentType = Inventor.DocumentTypeEnum.kPartDocumentObject
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Shared Function IsAssembly(document As Inventor.Document) As Boolean
+
+        If document.DocumentType = Inventor.DocumentTypeEnum.kAssemblyDocumentObject
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Shared Function IsDrawing(document As Inventor.Document) As Boolean
+
+        If document.DocumentType = Inventor.DocumentTypeEnum.kDrawingDocumentObject
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Shared Function IsPresentation(document As Inventor.Document) As Boolean
+
+        If document.DocumentType = Inventor.DocumentTypeEnum.kPresentationDocumentObject
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Shared Function IsForeignModel(document As Inventor.Document) As Boolean
+
+        If document.DocumentType = Inventor.DocumentTypeEnum.kForeignModelDocumentObject
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Shared Function IsNest(document As Inventor.Document) As Boolean
+'        Try
+'        If document.DocumentType = Inventor.DocumentTypeEnum.kNestingDocument
+'            Return True
+'        End If
+'        Catch
+'            End Try
+        Return False
+    End Function
+
+    Shared Function IsSAT(document As Inventor.Document) As Boolean
+        
+        If document.DocumentType = Inventor.DocumentTypeEnum.kSATFileDocumentObject
+            Return True
+        End If
+
+
+        Return False
+    End Function
+
+    Shared Function IsUnknown(document As Inventor.Document) As Boolean
+
+        If document.DocumentType = Inventor.DocumentTypeEnum.kUnknownDocumentObject
+            Return True
+        End If
+
+        Return False
+    End Function
+
+
+End Class
---
+++ tests/DocumentShim.Test.vb
@@ -0,0 +1,19 @@
+﻿Imports InventorShims
+Imports Inventor
+AddVbFile "src/DocumentShim.vb"
+
+'Run the PropertyShim functions from inventor's ilogic environment.
+'Public Module Test
+Sub Main()
+
+MsgBox("IsAssembly: " & DocumentShim.IsAssembly(ThisApplication.ActiveDocument) & vbLf & _
+            "IsDrawing: " & DocumentShim.IsDrawing(ThisApplication.ActiveDocument) & vbLf & _
+            "IsForeignModel: " & DocumentShim.IsForeignModel(ThisApplication.ActiveDocument) & vbLf & _
+            "IsNest: " & DocumentShim.IsNest(ThisApplication.ActiveDocument) & vbLf & _
+            "IsPart: " & DocumentShim.IsPart(ThisApplication.ActiveDocument) & vbLf & _    
+            "IsPresentation: " & DocumentShim.IsPresentation(ThisApplication.ActiveDocument) & vbLf & _
+            "IsSAT: " & DocumentShim.IsSAT(ThisApplication.ActiveDocument) & vbLf & _
+            "IsUnknown: " & DocumentShim.IsUnknown(ThisApplication.ActiveDocument))
+
+End Sub
+'End Module
