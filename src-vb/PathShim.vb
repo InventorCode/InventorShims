5b01760a5492adf7bcc76d731c4dfe946bde9a83
jordanrobot <meloncholia@gmail.com>
2020-11-24 19:08:11 -0500

    Added PathShim.UpOneLevel and corresponding unit test

---
+++ src-vb/PathShim.vb
@@ -0,0 +1,31 @@
+﻿Imports Inventor
+
+Public Class PathShim
+
+    Shared Function UpOneLevel(ByVal path As String, Optional delimiter As String = "\") As String
+
+        'fix paths with incorrect backslashes
+        If delimiter = "\" Then path = Replace(path, "/", "\")
+
+        'no slashes?  not a path...
+        If Not path.Contains(delimiter) Then
+            Return Nothing
+        End If
+
+        'Catch paths which end with a delimiter, such as C:\Work\Stuff\
+        'Clean up so that they look like C:\Work\Stuff
+        If path.EndsWith(delimiter) Then
+            path = Left(path, path.LastIndexOf(delimiter))
+        End If
+
+
+        Dim delimPos As Integer = path.LastIndexOf(delimiter)
+        If (delimPos <= 0) Then
+            Return Nothing
+        End If
+
+        Return Left(path, delimPos + 1)
+
+    End Function
+
+End Class
---
+++ tests/PathShim.Test.vb
@@ -0,0 +1,43 @@
+﻿Imports InventorShims
+Imports Inventor
+AddVbFile "src-vb/PathShim.vb"
+
+'Public Module Test
+Sub Main()
+
+    Dim testA = "C:\A\Test\String\"
+    Dim testB = PathShim.UpOneLevel(testA)
+    Logger.info("UpOneLevel Test 1 : " & If(testB.Equals("C:\A\Test\"), "Pass", "Fail"))
+    Logger.info("UpOneLevel Test 1 : " & testB)
+
+
+    testA = "C:\A\Test\String"
+    testB = PathShim.UpOneLevel(testA)
+    Logger.info("UpOneLevel Test 2 : " & If(testB.Equals("C:\A\Test\"), "Pass", "Fail"))
+    Logger.info("UpOneLevel Test 2 : " & testB)
+
+
+    testA = "C:"
+    testB = PathShim.UpOneLevel(testA)
+    Logger.info("UpOneLevel Test 3 : " & If(Equals(testB, Nothing), "Pass", "Fail"))
+    Logger.info("UpOneLevel Test 3 : " & testB)
+
+
+    testA = "C:\"
+    testB = PathShim.UpOneLevel(testA)
+    Logger.info("UpOneLevel Test 4 : " & If(Equals(testB, Nothing), "Pass", "Fail"))
+    Logger.info("UpOneLevel Test 4 : Nothing")
+
+
+    testA = "C:\A\"
+    testB = PathShim.UpOneLevel(testA)
+    Logger.info("UpOneLevel Test 5 : " & If(Equals(testB, "C:\"), "Pass", "Fail"))
+    Logger.info("UpOneLevel Test 5 : Nothing")
+
+    testA = "C\x"
+    testB = PathShim.UpOneLevel(testA)
+    Logger.info("UpOneLevel Test 6 : " & If(Equals(testB, "C\"), "Pass", "Fail"))
+    Logger.info("UpOneLevel Test 6 : " & testB)
+
+End Sub
+'End Module
