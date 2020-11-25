52c79d77d6e613e1c7c9074ecc0d264e76c300db
jordanrobot <meloncholia@gmail.com>
2020-11-25 00:47:04 -0500

    Added IsContentCenterPath and IsLibraryPath, as well as unit tests

--- src-vb/PathShim.vb
+++ src-vb/PathShim.vb
@@ -14,10 +14,7 @@ Public Class PathShim
 
         'Catch paths which end with a delimiter, such as C:\Work\Stuff\
         'Clean up so that they look like C:\Work\Stuff
-        If path.EndsWith(delimiter) Then
-            path = Left(path, path.LastIndexOf(delimiter))
-        End If
-
+        path = TrimTrailingSlash(path)
 
         Dim delimPos As Integer = path.LastIndexOf(delimiter)
         If (delimPos <= 0) Then
@@ -25,6 +22,53 @@ Public Class PathShim
         End If
 
         Return Left(path, delimPos + 1)
+
+    End Function
+
+    Shared Function IsLibraryPath(ByVal path As String, ByRef inventorApp As Inventor.Application) As Boolean
+
+        Dim designprojectmanager As DesignProjectManager = inventorApp.DesignProjectManager
+        Dim designproject As Inventor.DesignProject = designprojectmanager.ActiveDesignProject
+        Dim librarypaths As ProjectPaths = designproject.LibraryPaths
+        Dim librarypath As ProjectPath
+
+        For Each librarypath In librarypaths
+            If path.Contains(librarypath.Path) Then
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
+
+    Shared Function IsContentCenterPath(ByVal path As String, ByRef inventorApp As Inventor.Application) As Boolean
+
+        Dim designprojectmanager As DesignProjectManager = inventorApp.DesignProjectManager
+        Dim designproject As Inventor.DesignProject = designprojectmanager.ActiveDesignProject
+        Dim useProjectCCPath As Boolean = designproject.ContentCenterPathOverridden
+        Dim ccpath As String
+
+        If useProjectCCPath Then
+            ccpath = designproject.ContentCenterPath
+        Else
+            ccpath = inventorApp.FileOptions.ContentCenterPath
+        End If
+
+        ccpath = TrimTrailingSlash(ccpath)
+
+        If path.Contains(ccpath) Then
+            Return True
+        End If
+
+        Return False
+    End Function
+    Shared Function TrimTrailingSlash(ByVal path As String) As String
+
+        If path.EndsWith("\") Then
+            path = Left(path, path.LastIndexOf("\"))
+        End If
+
+        Return path
 
     End Function
 
