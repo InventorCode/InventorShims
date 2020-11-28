16c3de0571f6fab27d1ed0135527c1a4934bed73
Matt <119146+jordanrobot@users.noreply.github.com>
2020-11-28 12:58:34 -0500

    Enabled Attribute compilation.  Fixed Attribuite Test.

--- tests/Attribute.Test.vb
+++ tests/Attribute.Test.vb
@@ -1,32 +1,31 @@
-﻿'Imports InventorShims
+﻿Imports InventorShims
 Imports Inventor
 AddVbFile "src-vb/Attribute.vb"
 
 'Public Module Test
 Sub Main()
 
-Dim doc As Inventor.Document = ThisApplication.ActiveDocument
+	Dim doc As Inventor.Document = ThisApplication.ActiveDocument
 
-' Get the selected entity.
-Dim entity As Object = doc.SelectSet.Item(1)
+	' Get the selected entity.
 
-Dim attributeSetExistAnswer As Boolean
-	attributeSetExistAnswer = InventorShims.Attribute.DoesAttributeSetExist(entity, "GrandTest")
+	Dim attributeSetExistAnswer As Boolean
+	attributeSetExistAnswer = Attribute.DoesAttributeSetExist(doc, "GrandTest")
 
-	Attribute.SetAttribute(entity, "GrandTest", "testing", 2345)
+	Attribute.SetAttribute(doc, "GrandTest", "testing", 2345)
 
-	InventorShims.Attribute.SetAttribute(entity, "GrandTest", "testing", 123)
+	Attribute.SetAttribute(doc, "GrandTest", "testing", 123)
 
 	MsgBox("DoesAttributeSetExist: " & attributeSetExistAnswer & vbLf &
-			"DoesAttributeSetExist after creation?: " & InventorShims.Attribute.DoesAttributeSetExist(entity, "GrandTest") & vbLf &
-			"Does attribute Exist after creation? " & InventorShims.Attribute.DoesAttributeExist(entity, "GrandTest", "testing") & vbLf &
-			"GetAttribute: " & InventorShims.Attribute.GetAttribute(entity, "GrandTest", "testing"))
+			"DoesAttributeSetExist after creation?: " & Attribute.DoesAttributeSetExist(doc, "GrandTest") & vbLf &
+			"Does attribute Exist after creation? " & Attribute.DoesAttributeExist(doc, "GrandTest", "testing") & vbLf &
+			"GetAttribute: " & Attribute.GetAttribute(doc, "GrandTest", "testing"))
 
-	InventorShims.Attribute.RemoveAttribute(entity, "GrandTest", "testing")
-	InventorShims.Attribute.RemoveAttributeSet(entity, "GrandTest")
+	Attribute.RemoveAttribute(doc, "GrandTest", "testing")
+	Attribute.RemoveAttributeSet(doc, "GrandTest")
 
-	MsgBox("Does attribute exist after removal? : " & InventorShims.Attribute.DoesAttributeExist(entity, "GrandTest", "testing") & vbLf &
-		"Does attributeSet exist after removal? : " & InventorShims.Attribute.DoesAttributeSetExist(entity, "GrandTest"))
+	MsgBox("Does attribute exist after removal? : " & Attribute.DoesAttributeExist(doc, "GrandTest", "testing") & vbLf &
+			"Does attributeSet exist after removal? : " & Attribute.DoesAttributeSetExist(doc, "GrandTest"))
 
 
 End Sub
