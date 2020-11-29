e4c61ab2d988166bb6c3dfed4d78229ef13f8480
DSikes <pockybum522@gmail.com>
2020-11-29 09:06:58 -0500

    Added names of from units and added millimeter conversions

--- src-cs/Conversions.cs
+++ src-cs/Conversions.cs
@@ -11,36 +11,107 @@ namespace InventorShims
     /// </summary>
     public static class Conversions
     {
+        #region InchesTo
         /// <summary>
-        /// Handles extending a double and returns that double converted to centimeters
+        /// Handles extending an inches double and returns that double converted to centimeters
         /// </summary>
         /// <param name="inputNum"></param>
         /// <returns></returns>
-        public static double ToCentimeters(this double inputNum)
+        public static double InchesToCentimeters(this double inputNum)
         {
             return inputNum * 2.54;
         }
 
         // Int overload of the above
-        public static double ToCentimeters(this int inputNum)
+        public static double InchesToCentimeters(this int inputNum)
         {
             return inputNum * 2.54;
         }
 
+        /// <summary>
+        /// Handles extending an inches double and returns that double converted to millimeters
+        /// </summary>
+        /// <param name="inputNum"></param>
+        /// <returns></returns>
+        public static double InchesToMillimeters(this double inputNum)
+        {
+            return InchesToCentimeters(inputNum) * 10;
+        }
+
+        // Int overload of the above
+        public static double InchesToMillimeters(this int inputNum)
+        {
+            return InchesToCentimeters(inputNum) * 10;
+        }
+        #endregion
+
+        #region CentimetersTo
         /// <summary>
         /// Handles extending a double and returns that double converted to inches
         /// </summary>
         /// <param name="inputNum"></param>
         /// <returns></returns>
-        public static double ToInches(this double inputNum)
+        public static double CentimetersToInches(this double inputNum)
         {
             return inputNum / 2.54;
         }
 
         // Int overload of the above
-        public static double ToInches(this int inputNum)
+        public static double CentimetersToInches(this int inputNum)
         {
             return inputNum / 2.54;
         }
+
+        /// <summary>
+        /// Handles extending a double and returns that double converted to millimeters
+        /// </summary>
+        /// <param name="inputNum"></param>
+        /// <returns></returns>
+        public static double CentimetersToMillimeters(this double inputNum)
+        {
+            return inputNum * 10;
+        }
+
+        // Int overload of the above
+        public static double CentimetersToMillimeters(this int inputNum)
+        {
+            return inputNum * 10;
+        }
+        #endregion
+
+        #region MillimetersTo
+
+        /// <summary>
+        /// Handles extending a double and returns that double converted to millimeters
+        /// </summary>
+        /// <param name="inputNum"></param>
+        /// <returns></returns>
+        public static double MillimetersToInches(this double inputNum)
+        {
+            return inputNum * 2.54 * 10;
+        }
+
+        // Int overload of the above
+        public static double MillimetersToInches(this int inputNum)
+        {
+            return inputNum * 2.54 * 10;
+        }
+
+        /// <summary>
+        /// Handles extending a double and returns that double converted to millimeters
+        /// </summary>
+        /// <param name="inputNum"></param>
+        /// <returns></returns>
+        public static double MillimetersToCentimeters(this double inputNum)
+        {
+            return inputNum / 10;
+        }
+
+        // Int overload of the above
+        public static double MillimetersToCentimeters(this int inputNum)
+        {
+            return inputNum / 10;
+        }
+        #endregion
     }
 }
