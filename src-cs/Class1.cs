0e27308774e1ac8e453ab5aeeb0f9ab428507779
Matt <119146+jordanrobot@users.noreply.github.com>
2020-11-28 21:34:58 -0500

    Added cs project, merging of vb & cs build.
    * Closes #9 & #10.
    * Merges build output into a single InventorShims.dll
    * Added a rough C# project

---
+++ src-cs/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventor;
+using InventorShims;
+
+namespace InventorShims
+{
+    public static class TestClass
+    {
+        public static string Test()
+        {
+            return PathShim.UpOneLevel("C:/A/Test/Path/And/Stuff");
+        }
+    }
+}
