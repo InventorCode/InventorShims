e6fc3590f8725eb1fb4a1aae4bce76d690dc9c10
DSikes <pockybum522@gmail.com>
2020-11-29 08:01:17 -0500

    Deleted test classes, added conversions class
    And documentation!

---
+++ src-cs/Conversions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorShims
+{
+    /// <summary>
+    /// Conversions class handles easily converting numbers to useful things, like Inches > Centimeters, for example
+    /// </summary>
+    public static class Conversions
+    {
+        /// <summary>
+        /// Handles extending a double and returns that double converted to centimeters
+        /// </summary>
+        /// <param name="inputNum"></param>
+        /// <returns></returns>
+        public static double ToCentimeters(this double inputNum)
+        {
+            return inputNum * 2.54;
+        }
+
+        /// <summary>
+        /// Handles extending a double and returns that double converted to inches
+        /// </summary>
+        /// <param name="inputNum"></param>
+        /// <returns></returns>
+        public static double ToInches(this double inputNum)
+        {
+            return inputNum / 2.54;
+        }
+    }
+}
