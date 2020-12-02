e70c1690b20b53c5b55d24a74c9dd957683afdd4
Matt <119146+jordanrobot@users.noreply.github.com>
2020-12-02 00:29:43 -0500

    migrated PropertyShims to C#.
    Added a few more methods.  Spruced up unit tests.

---
+++ tests/iProperty.Unit Tests.vb
@@ -0,0 +1,25 @@
+﻿AddReference "InventorShims.dll"
+Imports InventorShims.Extension_Collections.PropertyShims
+'Run this test from inventor's ilogic environment.
+
+	Dim oDoc As Inventor.Document = ThisApplication.ActiveDocument
+    oDoc.SetProperty("Title", "Stuffffff")
+    oDoc.SetProperty("Things", "Stuffffff2")
+    oDoc.SetProperty("Custom Prop Set", "Things", "Stufzzzzzz")
+
+    MessageBox.Show(oDoc.GetProperty("Title"),"Stuffffff")
+    MessageBox.Show(oDoc.GetProperty("Things"),"Stuffffff2")
+    MessageBox.Show(oDoc.GetProperty("Custom Prop Set", "Things"), "Stufzzzzzz")
+
+	MsgBox("Does title exist? (yes): " & oDoc.PropertyExists("Title"))
+	MsgBox("Does things exist? (yes): " & oDoc.PropertyExists("Things"))
+	MsgBox("Does custom things exist? (yes): " & oDoc.PropertyExists("Custom Prop Set", "Things"))
+
+
+	oDoc.SetProperty("Title", "Title")
+    oDoc.RemoveProperty("Things")
+    oDoc.RemoveProperty("Custom Prop Set", "Things")
+
+	MsgBox("Does title exist? (yes): " & oDoc.PropertyExists("Title"))
+	MsgBox("Does things exist? (no): " & oDoc.PropertyExists("Things"))
+	MsgBox("Does custom things exist? (no): " & oDoc.PropertyExists("Custom Prop Set", "Things"))
