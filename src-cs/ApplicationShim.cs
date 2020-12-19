3d8fd52a04672aef7845834602b1e287256c9b53
Matt <119146+jordanrobot@users.noreply.github.com>
2020-12-18 23:14:47 -0500

    GetInventor replaced with ApplicationShim
    - No longer a singleton object
    - Instance method
    - NewInstance method
    - CurrentInstance method

---
+++ src-cs/ApplicationShim.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Inventor;
+
+namespace InventorShims
+{
+    public static class ApplicationShim
+    {
+
+        public static Inventor.Application Instance()
+        {
+            Inventor.Application app = null;
+
+            try
+            {
+                app = (Inventor.Application) Marshal.GetActiveObject("Inventor.Application");
+                return app;
+            }
+            catch (Exception e)
+            {
+                // is inventor not visible/in interactive mode?
+            }
+
+            //perhaps inventor needs to be started...
+            if (app == null)
+            {
+
+                try //to start inventor
+                {
+                    Type appType = Type.GetTypeFromProgID("Inventor.Application");
+                    app = (Inventor.Application)Activator.CreateInstance(appType);
+                    app.Visible = true;
+                    return app;
+                }
+                catch (Exception e2)
+                {
+                    MessageBox.Show("Unable to start Inventor.  Error message: " + e2.Message);
+                }
+
+            }
+
+            return null;
+        }
+
+        public static Inventor.Application CurrentInstance()
+        {
+            Inventor.Application app = null;
+
+            try
+            {
+                app = (Inventor.Application)Marshal.GetActiveObject("Inventor.Application");
+                return app;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Unable to get Inventor instance.  Error message: " + e.Message);
+            }
+
+            return null;
+        }
+
+        public static Inventor.Application NewInstance()
+        {
+            Inventor.Application app = null;
+
+                try //to start inventor
+                {
+                    Type appType = Type.GetTypeFromProgID("Inventor.Application");
+                    app = (Inventor.Application)Activator.CreateInstance(appType);
+                    app.Visible = false;
+                    return app;
+                }
+                catch (Exception e2)
+                {
+                    MessageBox.Show("Unable to start Inventor.  Error message: " + e2.Message);
+                }
+
+
+            return null;
+        }
+
+
+        //Code to open Apprentice Server below...
+        //Inventor.ApprenticeServerComponent oSvr = new Inventor.ApprenticeServerComponent();
+        //Inventor.ApprenticeServerDocument oAppDoc;
+        //Inventor.ApprenticeServerDrawingDocument oDwgDoc;
+
+        //    ApprenticeServerDocument document = _apprentice.Open(@"T:\$Work\Part1.ipt");
+        //    document.Close();
+        //    _apprentice = null;
+        //}
+        //catch (System.Exception e) { }
+    }
+
+}
+
---
+++ tests/ApplicationShimTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq.Expressions;
+using Inventor;
+using InventorShims;
+
+namespace tests
+{
+    [TestClass]
+    public class ApplicationShimTests
+    {
+            [TestMethod]
+            public void GetInstance_GetsOrStartsInstance()
+            {
+
+                Inventor.Application app = ApplicationShim.Instance();
+                app.Visible = true;
+
+
+                var testVariable = app.AssemblyOptions.DeferUpdate;
+
+                Assert.IsNotNull(testVariable);
+
+                app.Quit();
+                app = null;
+
+            }
+
+            [TestMethod]
+            public void CurrentInstance_NoInventorInstance_ReturnsNothing()
+            {
+                Inventor.Application app = null;
+                app = ApplicationShim.CurrentInstance();
+
+                try
+                {
+                    Assert.IsNull(app);
+                }
+                catch (AssertFailedException e)
+                {
+                    if (app != null)
+                    {
+                        app.Quit();
+                        app = null;
+                    }
+                }
+
+            }
+
+            [TestMethod]
+            public void NewInstance_StartsNewInventorInstance()
+            {
+                Inventor.Application app = null;
+                app = ApplicationShim.NewInstance();
+
+                try
+                {
+                    Assert.IsNotNull(app);
+                }
+
+                finally
+                {
+                    if (app != null)
+                    {
+                        app.Quit();
+                        app = null;
+                    }
+                }
+
+            }
+
+    }
+
+}
+
