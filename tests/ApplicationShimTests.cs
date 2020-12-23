840750ba5be113275c1f10f479eeda6557116e88
Matt <119146+jordanrobot@users.noreply.github.com>
2020-12-23 00:53:20 -0500

    cleaned up tests

--- tests/ApplicationShimTests.cs
+++ tests/ApplicationShimTests.cs
@@ -26,40 +26,6 @@ namespace ApplicationShimTests
         }
     }
 
-    [TestClass]
-    public class CurrentInstance
-    {
-
-
-        [TestMethod]
-        public void CurrentInstance_NoInventorInstance_ReturnsNothing()
-        {
-            Inventor.Application app = null;
-            app = ApplicationShim.CurrentInstance();
-
-            if (app != null)
-            {
-                app.Quit();
-                app = null;
-            }
-
-            app = ApplicationShim.CurrentInstance();
-
-            try
-            {
-                Assert.IsNull(app);
-            }
-            catch (AssertFailedException e)
-            {
-                if (app != null)
-                {
-                    app.Quit();
-                    app = null;
-                }
-            }
-
-        }
-    }
 
     [TestClass]
     public class NewInstance {
