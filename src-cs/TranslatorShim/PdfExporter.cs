90715493a0701283b766613ef98f0b5d654cf094
nannerdw <nannerdw@users.noreply.github.com>
2020-12-28 19:25:41 -0600

    Update PdfExporter.cs
    Replaced Export() with ExportSheet() and ExportSheets() methods

--- src-cs/TranslatorShim/PdfExporter.cs
+++ src-cs/TranslatorShim/PdfExporter.cs
@@ -7,64 +7,39 @@ using Inventor;
 
 namespace InventorShims.TranslatorShim
 {
-    /// <summary>Exports a drawing document to a PDF file</summary>
+    ///<summary>Exports a drawing document to a single PDF file</summary>
     public class PdfExporter
     {
         ///<summary>The drawing document to be exported</summary>
         public Inventor.Document Document { get; set; } = null;
 
+        ///<summary>Returns the document's fullfilename with the extension replaced by pdf</summary>
+        private string FullFileNameAsPdf
+        {
+            get
+            {
+                return System.IO.Path.ChangeExtension(this.Document.FullFileName, "pdf");
+            }
+        }
+
+        ///<summary>Exports in monochrome</summary>
         public bool AllColorsAsBlack { get; set; } = false;
 
+        ///<summary>Exports all lines to have the thinnest lineweight possible</summary>
         public bool RemoveLineWeights { get; set; } = false;
 
-        /// <value>dots per inch</value>
+        ///<summary>Determines how closely curved edges are approximated by straight line segments</summary>
+        ///<value>dots per inch</value>
         public int VectorResolution { get; set; } = 1200;
 
-        /// <summary>determines which sheets to include in the exported file</summary>
-        public PrintRangeEnum SheetRangeType { get; set; } = PrintRangeEnum.kPrintAllSheets;
-
-        ///<summary>backing variable for <see cref="SheetRangeStart"/></summary>
-        private int _sheetRangeStart = 1;
-
-        /// <summary>First sheet in range to include in export</summary>
-        /// <value>
-        /// Modifying this value automatically changes <see cref="SheetRangeType"/> to <see cref="PrintRangeEnum.kPrintSheetRange"/>
-        /// </value>
-        public int SheetRangeStart
-        {
-            get { return _sheetRangeStart; }
-
-            set
-            {
-                _sheetRangeStart = value;
-
-                SheetRangeType = PrintRangeEnum.kPrintSheetRange;
-            }
-        }
-
-        ///<summary>backing variable for <see cref="SheetRangeEnd"/></summary>
-        private int _sheetRangeEnd = 1;
-
-        /// <summary>Last sheet in range to include in export</summary>
-        /// <value>
-        /// Modifying this value automatically changes <see cref="SheetRangeType"/> to <see cref="PrintRangeEnum.kPrintSheetRange"/>
-        /// </value>
-        public int SheetRangeEnd
-        {
-            get { return _sheetRangeEnd; }
-
-            set
-            {
-                _sheetRangeEnd = value;
-
-                SheetRangeType = PrintRangeEnum.kPrintSheetRange;
-            }
-        }
-
-        /// <summary>Opens the PDF file in its default viewer after it has been created.</summary>
+        ///<summary>Opens the PDF file in its default viewer after it has been created</summary>
         public bool OpenWhenDone { get; set; } = false;
 
-        /// <remarks>This makes temporary edits to the file in memory, which may cause an unwanted Vault checkout prompt to appear if the drawing file is not checked out.</remarks>
+        ///<summary>Includes sheets in the exported file, even if they have the "Exclude fron printing" option checked.</summary>
+        ///<remarks>
+        ///This export option applies to any of the <see cref="ExportSheets()"/> methods, but not to the <see cref="ExportSheet()"/> methods.<br/>
+        ///This makes temporary edits to the file in memory, which may cause an unwanted Vault checkout prompt to appear if the drawing file is not checked out.
+        ///</remarks>
         public bool PrintExcludedSheets { get; set; } = false;
 
         ///<summary>Initializes a new instance of <see cref="PdfExporter"/></summary>
@@ -73,14 +48,191 @@ namespace InventorShims.TranslatorShim
             this.Document = (Document)Document;
         }
 
-        ///<summary>Export drawing document to PDF file with the same folder and filename as the document.</summary>
-        public void Export()
+        ///<summary>Export current sheet to a single PDF file with the same folder and filename as the document.</summary>
+        public void ExportSheet()
         {
-            Export(System.IO.Path.ChangeExtension(this.Document.FullFileName, "pdf"));
+            Export
+            (
+                OutputFile: FullFileNameAsPdf,
+                SheetRangeType: PrintRangeEnum.kPrintCurrentSheet
+            );
         }
 
-        ///<summary>Export drawing document to PDF file with the specified full file path.</summary>
-        public void Export(string OutputFile)
+        ///<summary>Export current sheet to a single PDF file with the specified full file path.</summary>
+        public void ExportSheet(string OutputFile)
+        {
+            Export
+            (
+                OutputFile: OutputFile,
+                SheetRangeType: PrintRangeEnum.kPrintCurrentSheet
+            );
+        }
+
+        ///<summary>Export specified sheet to a single PDF file with the same folder and filename as the document.</summary>
+        public void ExportSheet(int SheetNumber)
+        {
+            Export
+            (
+                OutputFile: FullFileNameAsPdf,
+                SheetRangeType: PrintRangeEnum.kPrintSheetRange,
+                StartSheet: SheetNumber,
+                EndSheet: SheetNumber
+            );
+        }
+
+        ///<summary>Export specified sheet to a single PDF file with the specified full file path.</summary>
+        public void ExportSheet(int SheetNumber, string OutputFile)
+        {
+            Export
+            (
+                OutputFile: OutputFile,
+                SheetRangeType: PrintRangeEnum.kPrintSheetRange,
+                StartSheet: SheetNumber,
+                EndSheet: SheetNumber
+            );
+        }
+
+        ///<summary>Export all sheets in document to a single PDF file with the same folder and filename as the document.</summary>
+        public void ExportSheets()
+        {
+            Export
+            (
+                OutputFile: FullFileNameAsPdf,
+                SheetRangeType: PrintRangeEnum.kPrintAllSheets
+            );
+        }
+
+        ///<summary>Export all sheets in document to a single PDF file with the specified full file path.</summary>
+        public void ExportSheets(string OutputFile)
+        {
+            Export
+            (
+                OutputFile: OutputFile,
+                SheetRangeType: PrintRangeEnum.kPrintAllSheets
+            );
+        }
+
+        ///<summary>Export sheet range to a single PDF file with the same folder and filename as the document.</summary>
+        public void ExportSheets(int StartSheet, int EndSheet)
+        {
+            Export
+            (
+                OutputFile: FullFileNameAsPdf,
+                SheetRangeType: PrintRangeEnum.kPrintSheetRange,
+                StartSheet: StartSheet, 
+                EndSheet: EndSheet
+            );
+        }
+
+        ///<summary>Export sheet range to a single PDF file with the specified full file path.</summary>
+        public void ExportSheets(int StartSheet, int EndSheet, string OutputFile)
+        {
+            Export
+            (
+                OutputFile: OutputFile,
+                SheetRangeType: PrintRangeEnum.kPrintSheetRange,
+                StartSheet: StartSheet,
+                EndSheet: EndSheet
+            );
+        }
+
+        ///<summary>Export all sheets, beginning with <paramref name="StartSheet"/>, to a single PDF file with the same folder and filename as the document.</summary>
+        public void ExportSheets(int StartSheet)
+        {
+            DrawingDocument dwgDoc = (DrawingDocument)this.Document;
+
+            Export
+            (
+                OutputFile: FullFileNameAsPdf,
+                SheetRangeType: PrintRangeEnum.kPrintSheetRange,
+                StartSheet: StartSheet,
+                EndSheet: dwgDoc.Sheets.Count
+            );
+        }
+
+        ///<summary>Export all sheets, beginning with <paramref name="StartSheet"/>, to a single PDF file with the specified full file path.</summary>
+        public void ExportSheets(int StartSheet, string OutputFile)
+        {
+            DrawingDocument dwgDoc = (DrawingDocument)this.Document;
+
+            Export
+            (
+                OutputFile: OutputFile,
+                SheetRangeType: PrintRangeEnum.kPrintSheetRange,
+                StartSheet: StartSheet,
+                EndSheet: dwgDoc.Sheets.Count
+            );
+        }
+
+        ///<summary>Export specific sheets to a single PDF file with the same folder and filename as the document.</summary>
+        ///<remarks>
+        ///The option <see cref="PrintExcludedSheets"/> still applies when using this method.<br/>
+        ///This makes temporary edits to the file in memory, which may cause an unwanted Vault checkout prompt to appear if the drawing file is not checked out.
+        ///</remarks>
+        public void ExportSheets(IEnumerable<Sheet> Sheets)
+        {
+            Export
+            (
+                OutputFile: FullFileNameAsPdf,
+                SheetRangeType: PrintRangeEnum.kPrintAllSheets,
+                IncludedSheets: Sheets
+            );
+        }
+
+        ///<summary>Export specific sheets to a single PDF file with the specified full file path.</summary>
+        ///<remarks>
+        ///The option <see cref="PrintExcludedSheets"/> still applies when using this method.<br/>
+        ///This makes temporary edits to the file in memory, which may cause an unwanted Vault checkout prompt to appear if the drawing file is not checked out.
+        ///</remarks>
+        public void ExportSheets(IEnumerable<Sheet> Sheets, string OutputFile)
+        {
+            Export
+            (
+                OutputFile: OutputFile,
+                SheetRangeType: PrintRangeEnum.kPrintAllSheets,
+                IncludedSheets: Sheets
+            );
+        }
+
+        ///<summary>Export specific sheet numbers to a single PDF file with the same folder and filename as the document.</summary>
+        ///<remarks>
+        ///The option <see cref="PrintExcludedSheets"/> still applies when using this method.<br/>
+        ///This makes temporary edits to the file in memory, which may cause an unwanted Vault checkout prompt to appear if the drawing file is not checked out.
+        ///</remarks>
+        public void ExportSheets(IEnumerable<int> SheetNumbers)
+        {
+            Export
+            (
+                OutputFile: FullFileNameAsPdf,
+                SheetRangeType: PrintRangeEnum.kPrintAllSheets,
+                IncludedSheetNums: SheetNumbers
+            );
+        }
+
+        ///<summary>Export specific sheet numbers to a single PDF file with the specified full file path.</summary>
+        ///<remarks>
+        ///The option <see cref="PrintExcludedSheets"/> still applies when using this method.<br/>
+        ///This makes temporary edits to the file in memory, which may cause an unwanted Vault checkout prompt to appear if the drawing file is not checked out.
+        ///</remarks>
+        public void ExportSheets(IEnumerable<int> SheetNumbers, string OutputFile)
+        {
+            Export
+            (
+                OutputFile: OutputFile,
+                SheetRangeType: PrintRangeEnum.kPrintAllSheets,
+                IncludedSheetNums: SheetNumbers
+            );
+        }
+
+        ///<summary>Export to a single PDF file</summary>
+        private void Export(
+            string OutputFile,
+            PrintRangeEnum SheetRangeType, 
+            int StartSheet = 0, 
+            int EndSheet = 0, 
+            IEnumerable<Sheet> IncludedSheets = null, 
+            IEnumerable<int> IncludedSheetNums = null
+            )
         {
             TranslatorData oTranslatorData = new TranslatorData(addinGUID: "{0AC6FD96-2F4D-42CE-8BE0-8AEA580399E4}", fullFileName: OutputFile, doc: this.Document);
 
@@ -90,29 +242,56 @@ namespace InventorShims.TranslatorShim
             op.Value["Remove_Line_Weights"] = Convert.ToInt32(RemoveLineWeights);
             op.Value["Vector_Resolution"] = Convert.ToInt32(VectorResolution);
             op.Value["Sheet_Range"] = SheetRangeType;
-            op.Value["Custom_Begin_Sheet"] = SheetRangeStart;
-            op.Value["Custom_End_Sheet"] = SheetRangeEnd;
+            if (StartSheet != 0) { op.Value["Custom_Begin_Sheet"] = StartSheet; }
+            if (EndSheet != 0) { op.Value["Custom_End_Sheet"] = EndSheet; }
             //op.Value["Launch_Viewer"] = Convert.ToInt32(OpenWhenDone);  //Does not work.  Workaround is at the bottom of this function.
-
-            Inventor.Application app = (Inventor.Application)this.Document.Parent;
 
             Transaction tempTransaction = null;
 
             //This is wrapped in a try/catch block to ensure the transaction gets aborted
             try
             {
+                Inventor.Application app = (Inventor.Application)this.Document.Parent;
+
+                DrawingDocument dwgDoc = (DrawingDocument)this.Document;
+
+                tempTransaction = app.TransactionManager.StartTransaction((Inventor._Document)this.Document, "Temporary Transaction");
+
                 if (PrintExcludedSheets)
                 {
-                    DrawingDocument dwgDoc = (DrawingDocument)this.Document;
-
+                    //Temporarily include all sheets.
                     List<Sheet> excludedSheets = dwgDoc.Sheets.OfType<Sheet>().Where(x => x.ExcludeFromPrinting).ToList();
 
                     if (excludedSheets.Count > 0)
                     {
-                        tempTransaction = app.TransactionManager.StartTransaction((Inventor._Document)this.Document, "Temporary Transaction");
-
                         excludedSheets.ForEach(x => x.ExcludeFromPrinting = false);
                     }
+                }
+
+                //Temporarily exclude sheets that were not passed in to this function.
+                if (IncludedSheets != null)
+                {
+                    List<Sheet> temporarilyExcludedSheets = dwgDoc.Sheets.OfType<Sheet>().Except(IncludedSheets).ToList();
+
+                    temporarilyExcludedSheets.ForEach(x => x.ExcludeFromPrinting = true);
+                }
+
+                //Temporarily exclude sheet numbers that were not passed in to this function.
+                if (IncludedSheetNums != null)
+                {
+                    List<Sheet> includedSheets = new List<Sheet>();
+
+                    for(int i = 1; i <= dwgDoc.Sheets.Count; i++)
+                    {
+                        if (IncludedSheetNums.Contains(i))
+                        {
+                            includedSheets.Add(dwgDoc.Sheets[i]);
+                        }
+                    }
+
+                    List<Sheet> temporarilyExcludedSheets = dwgDoc.Sheets.OfType<Sheet>().Except(includedSheets).ToList();
+
+                    temporarilyExcludedSheets.ForEach(x => x.ExcludeFromPrinting = true);
                 }
 
                 //Create output directory if it does not exist
@@ -132,13 +311,10 @@ namespace InventorShims.TranslatorShim
             }
 
             //Workaround for the PDF exporter's "Launch_Viewer" option, which does not work.
-            if (OpenWhenDone)
+            if (OpenWhenDone && System.IO.File.Exists(OutputFile))
             {
-                if(System.IO.File.Exists(OutputFile))
-                {
-                    //Open pdf file in its default application
-                    System.Diagnostics.Process.Start(OutputFile);
-                }
+                //Open pdf file in its default application
+                System.Diagnostics.Process.Start(OutputFile);
             }
         }
     }
