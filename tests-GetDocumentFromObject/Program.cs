989855580418c3d6ef4e0dcd8c833423b646a002
Matt <119146+jordanrobot@users.noreply.github.com>
2021-01-01 03:28:48 -0500

    Added tests for GetDocumentFromObject

---
+++ tests-GetDocumentFromObject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventor;
+using InventorShims;
+
+namespace GetDocumentFromObjectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        Inventor.Application oApp = ApplicationShim.CurrentInstance();
+        SelectSet oSSet = oApp.ActiveDocument.SelectSet;
+            if (oSSet.Count == 0) {
+                oSSet.Select(oApp.CommandManager.Pick(SelectionFilterEnum.kAllEntitiesFilter, "Select thing(s)..."));
+            }
+
+
+            if (oSSet.Count == 0) {
+                return;
+            }
+
+            List<Document> documentList = new List<Document>();
+    		documentList = DocumentShim.GetDocumentsFromSelectSet(oSSet);
+
+            //Get document from document, test...
+//        Console.WriteLine(DocumentShim.GetDocumentFromObject(oApp.ActiveDocument).FullDocumentName);
+
+            Console.WriteLine("Number of documents in List: " + documentList.Count().ToString());
+        foreach (Document i in documentList) {
+            //Console.WriteLine(i.FullFileName);
+            }
+         
+        Console.ReadLine();
+        }
+    }
+}
---
+++ tests/GetDocumentFromObjectTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InventorShims;
+using Inventor;
+
+namespace DocumentShim_Tests
+{
+    [TestClass]
+    public class ReturnDocumentFromObject_Tests
+    {
+        [TestMethod]
+        public void PartDocumentIn_ReturnsDocument()
+        {
+
+            Inventor.Application app = ApplicationShim.Instance();
+            var path = app.DesignProjectManager.ActiveDesignProject.TemplatesPath;
+            Document doc = app.Documents.Add(DocumentTypeEnum.kPartDocumentObject, path + "Standard.ipt", true);
+
+            var doc2 = doc.GetDocumentFromObject();
+
+            var tt = PropertyShim.GetPropertyValue(doc2, "Author");
+            //only exists in ParDocuments
+            PropertySets test = null;
+            try
+            {
+                test = doc2.PropertySets;
+            }
+            catch { }
+
+            try
+            {
+                Assert.IsNotNull(test);
+            }
+            finally { doc.Close(true); }
+        }
+
+        [TestMethod]
+        public void AssemblyDocumentIn_ReturnsDocument()
+        {
+
+            Inventor.Application app = ApplicationShim.Instance();
+            var path = app.DesignProjectManager.ActiveDesignProject.TemplatesPath;
+            Document doc = app.Documents.Add(DocumentTypeEnum.kAssemblyDocumentObject, path + "Standard.iam", true);
+
+            var doc2 = doc.GetDocumentFromObject();
+
+            var tt = PropertyShim.GetPropertyValue(doc2, "Author");
+            //only exists in ParDocuments
+            PropertySets test = null;
+            try
+            {
+                test = doc2.PropertySets;
+            }
+            catch { }
+
+            try
+            {
+                Assert.IsNotNull(test);
+            }
+            finally { doc.Close(true); }
+        }
+    }
+}
