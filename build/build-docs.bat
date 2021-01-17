ff9e01d6afb8c93e9974f233212a86d9c4a0d3b3
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-01-17 10:03:54 -0500

    docs: added --force to docfx rebuild.  Added new template reference for icons
    Closes #91
    Closes #92

--- build/build-docs.bat
+++ build/build-docs.bat
@@ -1,4 +1,4 @@
 REM Requires docfx installed to work...
 
-docfx ../docfx/docfx.json
+docfx ../docfx/docfx.json --force -t default,../docfx/templates/inventor-shims 
 PowerShell.exe -Command "& './replace-code-tags.ps1'"
