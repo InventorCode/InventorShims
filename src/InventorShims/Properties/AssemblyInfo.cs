df2cadccf71922f6bf60e3e5b7cf6536e14abeb2
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-02-11 21:08:27 -0500

    chore: add nuspec

--- src/InventorShims/Properties/AssemblyInfo.cs
+++ src/InventorShims/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("InventorShims")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A collection of extension APIs for Autodesk Inventor.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("InventorCode")]
 [assembly: AssemblyProduct("InventorShims")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
