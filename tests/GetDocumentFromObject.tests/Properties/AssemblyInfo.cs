969b463f650011579ee16506c23b6eb33f380cf6
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-09-01 22:36:35 -0400

    build: WIP update

--- tests/GetDocumentFromObject.tests/Properties/AssemblyInfo.cs
+++ tests/GetDocumentFromObject.tests/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0+15.Branch.master.Sha.c042bdaac806c8af120760bd09aefd43b37e4801")]
+// [assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0+8.Branch.master.Sha.d010438967853ade421db46b55992022bcb74d66")]
--- tests/InventorShims.tests/Properties/AssemblyInfo.cs
+++ tests/InventorShims.tests/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("9a4c4253-55ea-4218-9fe0-9e473562a096")]
 
-// [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0+15.Branch.master.Sha.c042bdaac806c8af120760bd09aefd43b37e4801")]
+// [assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0+8.Branch.master.Sha.d010438967853ade421db46b55992022bcb74d66")]
