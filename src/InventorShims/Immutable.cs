c645dcaef535b41860cfad5b65936219b15bbfde
Matt Jordan <119146+jordanrobot@users.noreply.github.com>
2022-01-04 00:36:21 -0600

    feat: add static methods to filter each doc type

--- src/InventorShims/Immutable.cs
+++ src/InventorShims/Immutable.cs
@@ -34,6 +34,7 @@ namespace InventorShims
         }
 
         #region IEnumerable<Document>
+
         public static IEnumerable<Document> GetAllReferencedDocuments(this Document document)
         {
             return (IEnumerable<Document>)document.AllReferencedDocuments;
@@ -104,6 +105,49 @@ namespace InventorShims
             return (IEnumerable<Document>)document.ReferencingDocuments;
         }
 
+        public static IEnumerable<AssemblyDocument> AssemblyDocuments(this IEnumerable<Document> documents)
+        {
+            foreach (Document document in documents)
+            {
+                if (document.IsAssembly())
+                    yield return (AssemblyDocument)document;
+            }
+
+            yield break;
+        }
+
+        public static IEnumerable<DrawingDocument> DrawingDocuments(this IEnumerable<Document> documents)
+        {
+            foreach (Document document in documents)
+            {
+                if (document.IsDrawing())
+                    yield return (DrawingDocument)document;
+            }
+
+            yield break;
+        }
+
+        public static IEnumerable<PresentationDocument> PresentationDocuments(this IEnumerable<Document> documents)
+        {
+            foreach (Document document in documents)
+            {
+                if (document.IsPresentation())
+                    yield return (PresentationDocument)document;
+            }
+
+            yield break;
+        }
+
+        public static IEnumerable<PartDocument> PartDocuments(this IEnumerable<Document> documents)
+        {
+            foreach (Document document in documents)
+            {
+                if (document.IsPart())
+                    yield return (PartDocument)document;
+            }
+
+            yield break;
+        }
         #endregion IEnumerable<Document>
 
         private static void temp(this Document document)
