d9cabee193007a5234574620aaad698a24c147c1
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2022-01-15 10:31:51 -0500

    tests: add ParameterShim tests

--- tests/InventorShims.tests/ParameterShimTests.cs
+++ tests/InventorShims.tests/ParameterShimTests.cs
@@ -53,6 +53,71 @@ namespace ParameterShim_Tests
     }
 
     [TestFixture]
+    public class GetParameters
+    {
+        [Test]
+        public void GetParametersPartDocument_returns_notNull()
+        {
+            var doc = tests.TestUtilities.CreatePartDocument();
+            var testDoc = (PartDocument)doc;
+
+            try
+            {
+                Assert.IsNotNull(testDoc.GetParameters());
+            }
+            finally { doc.Close(true); }
+        }
+
+        [Test]
+        public void GetParametersAssemblyDocument_returns_notNull()
+        {
+            var doc = tests.TestUtilities.CreateAssemblyDocument();
+            var testDoc = (AssemblyDocument)doc;
+
+            try
+            {
+                Assert.IsNotNull(testDoc.GetParameters());
+            }
+            finally { doc.Close(true); }
+        }
+        [Test]
+        public void GetParametersDrawingDocument_returns_notNull()
+        {
+            var doc = tests.TestUtilities.CreateDrawingDocument();
+            var testDoc = (DrawingDocument)doc;
+
+            try
+            {
+                Assert.IsNotNull(testDoc.GetParameters());
+            }
+            finally { doc.Close(true); }
+        }
+        [Test]
+        public void GetParametersDocument_returns_notNull()
+        {
+            var doc = tests.TestUtilities.CreateDrawingDocument();
+
+            try
+            {
+                Assert.IsNotNull(doc.GetParameters());
+            }
+            finally { doc.Close(true); }
+        }
+        [Test]
+        public void GetParametersUnsupportedDocument_returns_error()
+        {
+            var doc = tests.TestUtilities.CreatePresentationDocument();
+
+            try
+            {
+                Assert.IsNull(doc.GetParameters());
+            }
+            finally { doc.Close(true); }
+            
+        }
+    }
+
+    [TestFixture]
     public class SetParameterValue
     {
         [Test]
@@ -99,6 +164,67 @@ namespace ParameterShim_Tests
         }
 
         [Test]
+        public void NumericPart_CreatesNew()
+        {
+            var original = tests.TestUtilities.CreatePartDocument();
+            PartDocument doc = (PartDocument)original;
+
+            doc.SetParameterValue("testing", "16", "cm");
+
+            Parameter parameter = doc.ComponentDefinition.Parameters["testing"];
+            double testing = (double)parameter.Value;
+
+            try
+            {
+                Assert.AreEqual(16, testing);
+            }
+            finally
+            {
+                original.Close(true);
+            }
+        }
+        [Test]
+        public void NumericAssembly_CreatesNew()
+        {
+            var original = tests.TestUtilities.CreateAssemblyDocument();
+            AssemblyDocument doc = (AssemblyDocument)original;
+
+            doc.SetParameterValue("testing", "16", "cm");
+
+            Parameter parameter = doc.ComponentDefinition.Parameters["testing"];
+            double testing = (double)parameter.Value;
+
+            try
+            {
+                Assert.AreEqual(16, testing);
+            }
+            finally
+            {
+                original.Close(true);
+            }
+        }
+        [Test]
+        public void NumericDrawing_CreatesNew()
+        {
+            var original = tests.TestUtilities.CreateDrawingDocument();
+            DrawingDocument doc = (DrawingDocument)original;
+
+            doc.SetParameterValue("testing", "16", "cm");
+
+            Parameter parameter = doc.Parameters["testing"];
+            double testing = (double)parameter.Value;
+
+            try
+            {
+                Assert.AreEqual(16, testing);
+            }
+            finally
+            {
+                original.Close(true);
+            }
+        }
+
+        [Test]
         public void Text_CreatesNew()
         {
             var doc = tests.TestUtilities.CreatePartDocument();
@@ -116,6 +242,66 @@ namespace ParameterShim_Tests
             finally
             {
                 doc.Close(true);
+            }
+        }
+        [Test]
+        public void TextPart_CreatesNew()
+        {
+            var orig = tests.TestUtilities.CreatePartDocument();
+            PartDocument doc = (PartDocument)orig;
+
+            doc.SetParameterValue("testing", "This is a test");
+
+            Parameter parameter = doc.ComponentDefinition.Parameters["testing"];
+            var testing = parameter.Value;
+
+            try
+            {
+                Assert.AreEqual("This is a test", testing);
+            }
+            finally
+            {
+                orig.Close(true);
+            }
+        }
+        [Test]
+        public void TextAssembly_CreatesNew()
+        {
+            var orig = tests.TestUtilities.CreateAssemblyDocument();
+            AssemblyDocument doc = (AssemblyDocument)orig;
+
+            doc.SetParameterValue("testing", "This is a test");
+
+            Parameter parameter = doc.ComponentDefinition.Parameters["testing"];
+            var testing = parameter.Value;
+
+            try
+            {
+                Assert.AreEqual("This is a test", testing);
+            }
+            finally
+            {
+                orig.Close(true);
+            }
+        }
+        [Test]
+        public void TextDrawing_CreatesNew()
+        {
+            var orig = tests.TestUtilities.CreateDrawingDocument();
+            DrawingDocument doc = (DrawingDocument)orig;
+
+            doc.SetParameterValue("testing", "This is a test");
+
+            Parameter parameter = doc.Parameters["testing"];
+            var testing = parameter.Value;
+
+            try
+            {
+                Assert.AreEqual("This is a test", testing);
+            }
+            finally
+            {
+                orig.Close(true);
             }
         }
 
@@ -161,6 +347,52 @@ namespace ParameterShim_Tests
                 doc.Close(true);
             }
         }
+        [Test]
+        public void BoolPart_CreatesNew()
+        {
+            var orig = tests.TestUtilities.CreatePartDocument();
+            PartDocument doc = (PartDocument)orig;
+
+            doc.SetParameterValue("testing", true);
+
+            PartDocument part = (PartDocument)doc;
+            Parameter parameter = part.ComponentDefinition.Parameters["testing"];
+            var testing = parameter.Value;
+
+            try {Assert.AreEqual(true, testing);}
+            finally {orig.Close(true);}
+        }
+
+        [Test]
+        public void BoolAssembly_CreatesNew()
+        {
+            var orig = tests.TestUtilities.CreateAssemblyDocument();
+            var doc = (AssemblyDocument)orig;
+
+            doc.SetParameterValue("testing", true);
+
+            Parameter parameter = doc.ComponentDefinition.Parameters["testing"];
+            var testing = parameter.Value;
+
+            try { Assert.AreEqual(true, testing); }
+            finally { orig.Close(true); }
+        }
+
+        [Test]
+        public void BoolDrawing_CreatesNew()
+        {
+            var orig = tests.TestUtilities.CreateDrawingDocument();
+            var doc = (DrawingDocument)orig;
+
+            doc.SetParameterValue("testing", true);
+
+            Parameter parameter = doc.Parameters["testing"];
+            var testing = parameter.Value;
+
+            try { Assert.AreEqual(true, testing); }
+            finally { orig.Close(true); }
+        }
+
 
         [Test]
         public void Bool_OverwritesExisting()
